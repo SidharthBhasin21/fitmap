4170e111a0793ddc5999abae7b2542b97bf19c2b
Sidharth Bhasin <sid21bhasin@gmail.com>
2024-07-14 23:16:59 +0530

    initial setup, tailwind setup

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+    return (
+        <View className="flex-1 items-center justify-center bg-orange-300">
+            <Text className="text-white text-2xl">Home page</Text>
+        </View>
+    )
+}
+
+export default Index
